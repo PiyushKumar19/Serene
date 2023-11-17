2e273a62ad4537e8738a3f47dce61a920a0f8a99
Piyush <developerpiyush1102k2@gmail.com>
2023-11-18 00:46:49 +0530

    Structured the examples in good way.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component, useState } from 'react';
+import { Student, AppleProp } from './Student';
+
 
 function App() {
-  function apple() 
-  {
-    return alert("apple function called");
+  const [data, setData] = useState('Piyush'); // Created a state and initialized it
+
+  function apple() {
+    setData('Kumar');
+    // data changes
+    return alert(data);
   }
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      {/* While calling function with onclick use it as a variable like this or with arrow function */}
-      <button onClick={apple}>Click Me!</button>
-      <button onClick={()=>apple()}>Another Click Me!</button>
+
+      <div style={{ backgroundColor: 'skyblue' }}>
+        <h1 >State in functional component : {data}</h1>
+        <button onClick={apple}>Click Me!</button>
+      </div>
+
+      <div style={{ backgroundColor: 'skyblue', margin: 10 }}>
+        <h2>States in class components ;)</h2>
+        <h2 >Student data from Class Component :) <Student></Student></h2>
+      </div>
+
+      <div style={{backgroundColor: 'powderblue'}}>
+        <h3>Props in React :)</h3>
+        <AppleProp name={"Piu"} email={"Piyush@gmail.com"}>Click for props</AppleProp>
+      </div>
+
     </div>
   );
 }
