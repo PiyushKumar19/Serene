ca787b07508dfdf9aa513a8cadf4b84369b4ca1a
Piyush <developerpiyush1102k2@gmail.com>
2023-11-18 15:40:16 +0530

    Created a basic form that returns the entered data in console.

--- src/App.js
+++ src/App.js
@@ -2,33 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component, useState } from 'react';
 import { Student, AppleProp } from './Student';
+import { Case1 } from './TestCase';
 
 
 function App() {
   const [data, setData] = useState('Piyush'); // Created a state and initialized it
+
+  const [name, setName ] = useState("");
+  const [tnc, setTnc] = useState(false);
+  const [interest, setInterest] = useState("");
 
   function apple() {
     setData('Kumar');
     // data changes
     return alert(data);
   }
+
+  function getFormData(e) {
+    console.log(name, tnc, interest);
+    e.preventDefault();
+  }
   return (
     <div className="App">
-
-      <div style={{ backgroundColor: 'skyblue' }}>
-        <h1 >State in functional component : {data}</h1>
-        <button onClick={apple}>Click Me!</button>
-      </div>
-
-      <div style={{ backgroundColor: 'skyblue', margin: 10 }}>
-        <h2>States in class components ;)</h2>
-        <h2 >Student data from Class Component :) <Student></Student></h2>
-      </div>
-
-      <div style={{backgroundColor: 'powderblue'}}>
-        <h3>Props in React :)</h3>
-        <AppleProp name={"Piu"} email={"Piyush@gmail.com"}>Click for props</AppleProp>
-      </div>
+      <Case1></Case1>
+      <form  onSubmit={getFormData}>
+        <input type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholders="Emter Name" />
+        <select onChange={(e)=>setInterest(e.target.value)}>
+          <option>Select Option</option>
+          <option>Marvel</option>
+          <option>DC</option>
+        </select>
+        <input type='checkbox' onChange={(e)=>setTnc(e.target.checked)} /><span>Accept Terms and conditions</span>
+        <button type='submit'>Submit</button>
+      </form>
 
     </div>
   );
